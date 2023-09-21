7badb4dad749deb03242c2234dab86f909610636
Чингис <cianeed08@gmail.com>
2023-09-21 03:06:03 +0300

    3 tasks has been solved

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Иванов";
+        String middleName = "Семён";
+        String lastName = "Семёнович";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        String withouYo = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + withouYo);
     }
 }
